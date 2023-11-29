27d62874fb35b50f034bfeeada593aa0de175f0d
Ricardo Amaya <ramaya@acr.org>
2023-11-29 00:25:36 -0500

    fixed issues with producer page

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,39 +4,36 @@ function onConnectionError(error) {
     console.log(`Mongo onnection error: ${error}`);
 }
 
-async function connect(connectionString) {
+async function connect() {
+
     try{
-        await mongoose.connect(connectionString);
+        //if full connection string is present in env then just use that to connect
+        if(process.env.MONGO_CONNECTION_STRING) {
+            await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+        } 
+        //otherwise build the connection parameters from parts and specify admin as auth source
+        else {
+            const dbUser = process.env.DB_USER
+            const dbPass = process.env.DB_PASS
+            const dbIp = process.env.DB_IP
+            const dbName = process.env.DB_NAME
+            await mongoose.connect(
+                `mongodb://${dbIp}/${dbName}`,
+                {
+                    authSource: "admin",
+                    user: `${dbUser}`,
+                    pass: `${dbPass}`
+                }
+            )
+        }
         console.log('mongodb connected');
     } catch (e) {
         onConnectionError(e);
     }
-    const connection = mongoose.connection;
-    connection.on("error", onConnectionError);
-    connection.once("open", () => console.log("Connected to DB!"));
-}
-
-async function connectWithAuth() {
-    const dbUser = process.env.DB_USER
-    const dbPass = process.env.DB_PASS
-    const dbIp = process.env.DB_IP
-    const dbName = process.env.DB_NAME
-
-    await mongoose.connect(
-        `mongodb://${dbIp}/${dbName}`,
-        {
-            authSource: "admin",
-            user: `${dbUser}`,
-            pass: `${dbPass}`
-        }
-    )
 
     const connection = mongoose.connection;
     connection.on("error", onConnectionError);
     connection.once("open", () => console.log("Connected to DB!"));
 }
 
-module.exports = {
-    connect: connect,
-    connectWithAuth: connectWithAuth
-}
+module.exports = connect;
--- src/server.js
+++ src/server.js
@@ -1,50 +1,46 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('node:path')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 
-require('dotenv').config()
 
 const pageRoutes = require('./routes/pages')
 const userRoutes = require('./routes/users')
 const apiRoutes = require('./routes/api')
 
 const dbConnection = require('./database/connection')
-const usc = require('./database/repositories/userRepository')
-
-const port = process.env.PORT
 
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
 }))
-
-dbConnection.connect(`${process.env.MONGO_CONNECTION_STRING}`)
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
 app.set(express.json())
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/pages/', pageRoutes)
-
 // Establish the user route
 app.use('/users/', userRoutes)
-
 app.use('/api/', apiRoutes)
-
-app.listen(port, () => {
-    console.log(`Server has started on port ${port}`)
-})
-
 app.get('/', (req, res) => {
     res.redirect('/pages/login')
 })
+
+async function main() {
+    await dbConnection(process.env.MONGO_CONNECTION_STRING)
+    app.listen(process.env.PORT);
+    console.log('Server is listening on port ' + process.env.PORT);
+}
+
+main().catch(err => console.log(err));
